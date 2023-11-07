ba89b04f1bdef03f6b3ac185db94285a66f0abae
ClairaVII <danghuy.pht@gmail.com>
2023-11-07 10:32:22 +0700

    Vi du ve 2 cach truy xuat

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const staff = {Name: "Lâm", ID: "2152xxxx", job:"Super idol"};
-        document.getElementById("demo").innerHTML = staff.Name + " có ID: " + staff.ID + " và là 1 " + staff.job;
+    document.getElementById("demo").innerHTML = staff.Name + " có ID: " + staff["ID"] + " và là 1 " + staff.job;
